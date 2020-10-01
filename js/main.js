f25f26652a93abf8e8ae7059e0b8967939c96de4
Lucia <lubersol@gmail.com>
2020-10-01 20:54:44 +0200

    añadido boton encender y otros efectos

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/* Elemento button que apaga la pantalla */
+var boton = document.getElementById("apagar");
+/* Se agrega el evento al elemento */
+boton.addEventListener("click", apagarPantalla);
+/* Función que se ejecuta al hacer click en el boton */
+
+function apagarPantalla {
+    if (boton.)
+}
+  
+
+
+
